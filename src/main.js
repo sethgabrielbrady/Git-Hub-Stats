bfbf129dfec7e36b351a1673c7841365ab713925
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-03-06 14:47:18 -0500

    main.js is pulling user data from github like from class

---
+++ src/main.js
@@ -0,0 +1,34 @@
+
+let fetch = require('node-fetch');
+
+let promise = fetch(
+    'https://api.github.com/users/jakerella',
+    {
+      method: 'GET',
+      headers: {
+         Authorization: 'token '  + process.argv[2]//PERSONAL access github token
+      },
+      body:'...'
+    }
+
+);//puts the request in
+
+
+//Your application should accept a username and API key as arguments
+
+/**********************************************START OF Question 1**********************/
+promise.then( function handleResponse(responseObj){// says, when the promise is ready, do somththing
+
+    console.log(responseObj.status);
+
+    if(responseObj.status > 199 && responseObj.status < 300 ){
+      //in here i know the request was succesul
+      responseObj.json().then( function printData(userData){
+        console.log(userData);//calls the entire object of user data
+      });
+    }else {
+      //now i know there is a problem
+      ////maybe we shoudl tell the user.
+      console.log('there was problem', responseObj.status );//error condition
+    }
+});
