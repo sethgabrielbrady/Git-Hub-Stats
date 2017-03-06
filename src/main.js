f098dfaff43ace3446803ddf094e615ac389ee38
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-03-06 17:17:55 -0500

    Send question answered

--- src/main.js
+++ src/main.js
@@ -2,17 +2,15 @@
 let fetch = require('node-fetch');
 
 let promise = fetch(
-    // 'https://api.github.com/users/' + process.argv[2],
+    'https://api.github.com/users/' + process.argv[2],
     'https://api.github.com/users/' + process.argv[2] + '/repos',
 
     {
       method: 'GET',
       headers: {
          Authorization: 'token '  + process.argv[3]//PERSONAL access github token
-      },
+      }
 
-
-      body:'...'
     }
 
 );//puts the request in
@@ -40,14 +38,27 @@ let promise = fetch(
 //
 //
 /**********************************************START OF Question 2**********************/
-promise.then( function handleResponse(responseObj){// says, when the promise is ready, do somththing
+  let count = 0;
+  let tempCount;
+promise.then(function handleResponse(responseObj){// says, when the promise is ready, do somththing
 
     console.log(responseObj.status);
+
 
     if(responseObj.status > 199 && responseObj.status < 300 ){
       //in here i know the request was succesul
       responseObj.json().then( function printData(userData){
-        console.log(userData[0].stargazers_count);//calls the entire object of user data
+        // console.log(userData[0].stargazers_count);//calls the entire object of user data
+        // console.log(userData[1]);
+      for (i=0; i<=userData.length; i++) {
+         console.log('length', userData.length);
+          console.log( userData[i]);
+          console.log( 'stars', userData[i].stargazers_count);
+          tempCount = userData[i].stargazers_count;
+          console.log('tempCount', typeof(tempCount));
+          count = count + Number(tempCount);
+          console.log('count', count);
+     }
 
       });
     }else {
