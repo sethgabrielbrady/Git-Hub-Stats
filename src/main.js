df0441945791b1398b74cd1ffb38c5e8f6f07d8b
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-03-06 15:13:08 -0500

    termial accepts a username along witht the code

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@
 let fetch = require('node-fetch');
 
 let promise = fetch(
-    'https://api.github.com/users/jakerella',
+     'https://api.github.com/users/' + process.argv[2],
+    // 'https://api.github.com/users/sethgabrielbrady/repos',
+
     {
       method: 'GET',
       headers: {
-         Authorization: 'token '  + process.argv[2]//PERSONAL access github token
+         Authorization: 'token '  + process.argv[3]//PERSONAL access github token
       },
       body:'...'
     }
@@ -24,7 +26,8 @@ promise.then( function handleResponse(responseObj){// says, when the promise is 
     if(responseObj.status > 199 && responseObj.status < 300 ){
       //in here i know the request was succesul
       responseObj.json().then( function printData(userData){
-        console.log(userData);//calls the entire object of user data
+        console.log(userData.name);//calls the entire object of user data
+        // console.log(userData.stargazers_count);
       });
     }else {
       //now i know there is a problem
@@ -32,3 +35,19 @@ promise.then( function handleResponse(responseObj){// says, when the promise is 
       console.log('there was problem', responseObj.status );//error condition
     }
 });
+
+
+
+//
+//
+// process.argv[2] == "month";
+// process.argv[3] == "year";
+//
+// let movingData = require('./moving.js');
+// // let movingAnalysis = movingData(process.argv[2], process.argv[3]); //not needed
+//
+// let parkingData = require('./parking.js');
+// // let parkingAnalysis = parkingData(process.argv[2], process.argv[3]); //not needed
+//
+// console.log(movingData(process.argv[2], process.argv[3]));
+// console.log(parkingData(process.argv[2], process.argv[3]));
