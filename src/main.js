e090c9143dcc35bd6f8b4a742801753564592c2f
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-03-06 15:52:39 -0500

    maybe figured out how to access the count in stargazers

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@
 let fetch = require('node-fetch');
 
 let promise = fetch(
-     'https://api.github.com/users/' + process.argv[2],
-    // 'https://api.github.com/users/sethgabrielbrady/repos',
+    // 'https://api.github.com/users/' + process.argv[2],
+    'https://api.github.com/users/' + process.argv[2] + '/repos',
 
     {
       method: 'GET',
       headers: {
          Authorization: 'token '  + process.argv[3]//PERSONAL access github token
       },
+
+
       body:'...'
     }
 
@@ -19,6 +21,25 @@ let promise = fetch(
 //Your application should accept a username and API key as arguments
 
 /**********************************************START OF Question 1**********************/
+// promise.then( function handleResponse(responseObj){// says, when the promise is ready, do somththing
+//
+//     console.log(responseObj.status);
+//
+//     if(responseObj.status > 199 && responseObj.status < 300 ){
+//       //in here i know the request was succesul
+//       responseObj.json().then( function printData(userData){
+//         console.log(userData.name);//calls the entire object of user data
+//         console.log(userData.login);
+//       });
+//     }else {
+//       //now i know there is a problem
+//       ////maybe we shoudl tell the user.
+//       console.log('there was problem', responseObj.status );//error condition
+//     }
+// });
+//
+//
+/**********************************************START OF Question 2**********************/
 promise.then( function handleResponse(responseObj){// says, when the promise is ready, do somththing
 
     console.log(responseObj.status);
@@ -26,8 +47,8 @@ promise.then( function handleResponse(responseObj){// says, when the promise is 
     if(responseObj.status > 199 && responseObj.status < 300 ){
       //in here i know the request was succesul
       responseObj.json().then( function printData(userData){
-        console.log(userData.name);//calls the entire object of user data
-        // console.log(userData.stargazers_count);
+        console.log(userData[0].stargazers_count);//calls the entire object of user data
+
       });
     }else {
       //now i know there is a problem
@@ -35,19 +56,3 @@ promise.then( function handleResponse(responseObj){// says, when the promise is 
       console.log('there was problem', responseObj.status );//error condition
     }
 });
-
-
-
-//
-//
-// process.argv[2] == "month";
-// process.argv[3] == "year";
-//
-// let movingData = require('./moving.js');
-// // let movingAnalysis = movingData(process.argv[2], process.argv[3]); //not needed
-//
-// let parkingData = require('./parking.js');
-// // let parkingAnalysis = parkingData(process.argv[2], process.argv[3]); //not needed
-//
-// console.log(movingData(process.argv[2], process.argv[3]));
-// console.log(parkingData(process.argv[2], process.argv[3]));
